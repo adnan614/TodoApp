49ef94e3f13a80de05da6cd9eee8e34ef096ad4b
Adnan Faruque Alvi <adnanfaruque23@gmail.com>
2021-05-04 17:57:41 +0600

    vuetodo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+    <h1 class="text-white display-4">Vue Js Todo APP</h1>
+    <example-component></example-component>
+</div>
+
+@stop
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/todo', [TodoController::class, 'store']);
+Route::get('/todo/show', [TodoController::class, 'index']);
+Route::post('/todo/update/{id}', [TodoController::class, 'update']);
+Route::post('/todo/delete/{id}', [TodoController::class, 'destroy']);
